7b20874116d69566dcf5832c270c98da67f87e92
abalan-rpx <abalan_c@rpxcorp.com>
2020-06-15 19:46:44 +0530

    initial deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn moneymanager.wsgi --log-file -
--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8agh9$h5iedqxer)1p)t4+&5y29lwt(*2pd2t5*bq#bf(o7hwb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moneymanager-achilles.herokuapp.com']
 
 
 # Application definition
@@ -83,15 +85,21 @@ WSGI_APPLICATION = 'moneymanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'moneymanager',
-        'USER' : 'moneymanager',
-        'PASSWORD' : 'moneymanager',
-        'HOST' : 'localhost',
-        'PORT' : '5432',    }
+    'default': config('DATABASE_URL', default=default_db_url, cast=dburl)
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'moneymanager',
+#         'USER' : 'moneymanager',
+#         'PASSWORD' : 'moneymanager',
+#         'HOST' : 'localhost',
+#         'PORT' : '5432',    }
+# }
 
 
 # Password validation
@@ -131,3 +139,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
